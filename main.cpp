fda5df6118bddc38c5db5e23367e4dcb64b81cc3
dsilvao <45110486+dsilvao@users.noreply.github.com>
2018-11-20 21:31:42 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Project 4a written by Darin Hunt & Ojasvi DSilva
+// Project 4b written by Darin Hunt & Ojasvi DSilva
 // This is the CPP file for Project 4. 
 // It calls the functions of the board class
 #include <iostream>
@@ -47,35 +47,22 @@ int main()
     {
     	
         board b1(SquareSize);
-        int countto81;
+  
         while (fin && fin.peek() != 'Z')
         {
+        	 
             b1.initialize(fin);
+            
             b1.print();
         }
         //while (b1.Solved()!=1)
         
         b1.printConflicts();
-        b1.setCell(3, 3, 7); //sets cell, for this purpose we only need to do it once
-        b1.print();
-        b1.printConflicts();
-       	b1.clearCell(3,3,7); //clears a cell for this purpose we only need to do it once
-       	b1.print();
-       	
-       	b1.printConflicts();
-        
-        //when calling the find first blank cell fucntion do:
-        //column = /*what is return from firstblankcell*/ /9 +1
-        //row = /*what is returned from first blankcell*/ %9 or if it is a x%9=0 number just make it equal to 9;
-        
-        solve=b1.Solved();
-        if (solve==true) //if solved is true it is solved
-        {
-        	cout << "Sodoku Solved" << endl;
-		}
-		else
-			cout << "Sodoku Not Solved" << endl;
-        
+        cout << "not solved";
+       	b1.solveBoard();
+		cout << " solved";
+     	b1.print();
+     	cout << " number of recursions " << b1.recursive << endl;
         
     }
     catch  (indexRangeError &ex)
